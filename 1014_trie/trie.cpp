47c8001570ca3d1a2aa5d3ee1c413d6639c9be86
sxysxy <1441157749@qq.com>
2017-02-12 10:23:23 +0800

    1014

--- 1014_trie/trie.cpp
+++ 1014_trie/trie.cpp
@@ -7,6 +7,31 @@
 #include <string>
 #include <vector>
 using namespace std;
+#define MAXS 26
+#define BASE 'a'
+struct node{
+  int cnt;     //标号记录
+  node *(next[26]);  //转移表
+  node(){memset(this, 0, sizeof(node));}
+}trie;
+void insert(node *u, char *c){
+  while(*c){
+    if(!u->next[*c-BASE])u->next[*c-BASE] = new node;
+    u->cnt++, u = u->next[*c-BASE]; c++;
+  }
+  u->cnt++;
+}
+int cnt(node *u, char *c){
+  while(*c){
+    if(!u->next[*c-BASE])return 0;
+    u = u->next[*c-BASE];c++;
+  }return u->cnt;
+}
+char tmp[20];
 int main(){
+  int n;scanf("%d", &n);
+  while(n--)scanf("%s", tmp), insert(&trie, tmp);
+  scanf("%d", &n);
+  while(n--)scanf("%s", tmp), printf("%d\n", cnt(&trie, tmp));
   return 0;
 }
