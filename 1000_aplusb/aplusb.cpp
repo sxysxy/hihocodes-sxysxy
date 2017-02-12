86e5824089086789cfa2155351faad6d2148ff4e
sxysxy <1441157749@qq.com>
2017-02-12 09:35:37 +0800

    ...

---
+++ 1000_aplusb/aplusb.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+int main(){
+  int a, b;
+  while(~scanf("%d %d", &a, &b))printf("%d\n", a+b);
+  return 0;
+}
---
+++ 1014_trie/trie.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+  return 0;
+}
